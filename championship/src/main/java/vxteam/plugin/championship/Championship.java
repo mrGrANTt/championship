f9ac327e5b1e9079799a13c1c8ae4eeae71e334d
mrGrANTt <sambistyyy0@gmail.com>
2024-10-07 13:25:34 +1000

    Project created!

---
+++ championship/src/main/java/vxteam/plugin/championship/Championship.java
@@ -0,0 +1,17 @@
+package vxteam.plugin.championship;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Championship extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
